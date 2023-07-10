6140b6d7d11cd2e8938e2bc27a8e8a110cc1e7aa
ahmet <ahmetdem6325@gmail.com>
2023-07-10 19:44:07 +0300

    first commit

--- src/week2/_1_Monday/Task1.java
+++ src/week2/_1_Monday/Task1.java
@@ -1,7 +1,12 @@
 package week2._1_Monday;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Task1 {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         // how to create new selenium project?
         // How to define the selenium libraries?
         // How to create Webdriver ?
@@ -12,7 +17,36 @@ public class Task1 {
         // How to send keys to specific element
         // how to define wait function?
         // How to quit driver?
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("incognito");
+        WebDriver driver = new ChromeDriver(options);
+        // Navigated to website
+        driver.get("https://katalon-demo-cura.herokuapp.com/");
+        //maximize
+        driver.manage().window().maximize();
 
+        WebElement makeAppointmentBtn=driver.findElement(By.id("btn-make-appointment"));
+        makeAppointmentBtn.click();
+        WebElement username = driver.findElement(By.id("txt-username"));
+
+        username.sendKeys("John Doe");
+        WebElement password=driver.findElement(By.id("txt-password"));
+        password.sendKeys("ThisIsNotAPassword");
+        driver.findElement(By.id("btn-login")).click();
+        WebElement bookApp=driver.findElement(By.id("btn-book-appointment"));
+        if (bookApp.getText().contains("Book")){
+            System.out.println(bookApp.getText());
+            System.out.println("You logged in");
+        }else {
+            System.out.println("attept failed");
+        }
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
     }
 
 }
