6df4c5bcdfcfe459c7894fe0140a92d995d3792b
emre087 <fee0984@gmail.com>
2023-07-15 11:09:00 +0200

    aa

---
+++ src/week2/_3_Wednesday/CssPractice.java
@@ -0,0 +1,17 @@
+package week2._3_Wednesday;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import week2.utilities.BaseDriver;
+import week2.utilities.MyFunctions;
+
+public class CssPractice extends BaseDriver {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+}
